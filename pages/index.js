7f27466f33db5c83e4684d4f655ced1ce5cad5dc
Kacper Zawojski <zawojweb@gmail.com>
2021-10-13 21:36:56 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,3 @@ export default function Home() {
     </main>
   )
 }
-
-{
-  /* <div class='max-w-lg mx-auto'>
-<div class='bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5'>
-  <a href='#'>
-    <img class='rounded-t-lg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Apache_Maven_logo.svg/1280px-Apache_Maven_logo.svg.png' alt='' />
-  </a>
-  <div class='p-5'>
-    <a href='#'>
-      <h5 class='text-gray-900 font-bold text-2xl tracking-tight mb-2'>Po co komu maven i jak to coś zainsatlować</h5>
-    </a>
-    <p class='font-normal text-gray-700 mb-3'>Instalacja nie jest trudna</p>
-    <Link href='/cours/mvn'>
-      <a class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center'>Poradnik</a>
-    </Link>
-  </div>
-</div>
-</div> */
-}
