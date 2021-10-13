4eabfd45e142d6ddb1557e6558616d0329b56027
Kacper Zawojski <zawojweb@gmail.com>
2021-10-13 20:58:00 +0200

    DepTest

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import Coureses from './components/Coureses'
 import Hero from './components/Hero'
 
 export default function Home() {
   return (
     <main className='bg-gray-800 min-h-screen'>
       <Hero dark={'Moje poradniki są'} blueText={'zajebiste'} text={'Tworze te poradniki aby pomóc moim szlachetnym i wspaniałym znajomym z studiów. Jak ci się nie podobają moje kursy to oby ci się nidy arch nie zainsatalował.'} img={'hero.png'} />
-      <div className='container mx-auto px-4'>
-        <Coureses />
-      </div>
+      <div className='container mx-auto px-4'></div>
     </main>
   )
 }
