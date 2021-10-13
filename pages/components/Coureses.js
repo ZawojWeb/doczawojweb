b4a41e334908543f72e3d3f9fed3e3fef3dd996f
Kacper Zawojski <zawojweb@gmail.com>
2021-10-13 21:30:53 +0200

    Update Coureses.js

--- pages/components/Coureses.js
+++ pages/components/Coureses.js
@@ -1,7 +1,7 @@
 import CoursCard from './CoursCard'
 export default function Coureses() {
   return (
-    <section className=''>
+    <section className='py-20'>
       <header className='py-12 text-white max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:text-center'>
         <h1 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl'>Poradniki i kursy na wszelakie tematy.</h1>
         <h2 className='mt-4 max-w-2xl text-xl lg:mx-auto'>Większości kursy są nie przydatne do życia, ale ułatwią ci przygotowanie do labów lub ćwików.</h2>
